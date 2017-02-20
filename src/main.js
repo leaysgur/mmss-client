e7c70ebf1fe5320ab70e5850313cf619fde425d2
leader22 <y.sugiura.0316@gmail.com>
2017-02-20 13:43:20 +0900

    build by webpack

---
+++ src/main.js
@@ -0,0 +1,7 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+
+const App = () => <div>Hello</div>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+
+const nodeEnv = process.env.NODE_ENV;
+const isProd = nodeEnv === 'production';
+
+console.log(`env: ${nodeEnv} -> isProd: ${isProd}`);
+
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+  }),
+];
+
+if (isProd) {
+  plugins.push(
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true,
+      },
+      output: {
+        comments: false,
+      },
+    })
+  );
+}
+
+module.exports = {
+  entry: {
+    main: './src/main.js',
+  },
+  output: {
+    filename: '[name]-bundle.js',
+    path: './',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: [/node_modules/],
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins,
+};
