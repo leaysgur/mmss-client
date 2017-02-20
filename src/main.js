26b170c4a09e96d6970dafe8bf2e62c5dd9612d3
leader22 <y.sugiura.0316@gmail.com>
2017-02-20 14:28:29 +0900

    fetch login check then boot

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+const LoginApp = () => <div>Login!</div>;
+const MmssApp = () => <div>App!</div>;
 
-const App = () => <div>Hello</div>;
+fetch('/api/check')
+  .then(res => res.json())
+  .then(res => {
+    if ('error' in res) {
+      ReactDOM.render(<LoginApp />, document.getElementById('root'));
+    } else {
+      ReactDOM.render(<MmssApp />, document.getElementById('root'));
+    }
+  })
+  .catch(console.error);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// const YYYYMMDD = new Date().toJSON().split('T')[0].split('-').join('');
+// fetch(`./dist/music.json?_=${YYYYMMDD}`)
+//   .then((res) => res.json())
+//   .then(() => {
+//     const App = () => <div>Hello</div>;
+//     ReactDOM.render(<App />, document.getElementById('root'));
+//   });
+
