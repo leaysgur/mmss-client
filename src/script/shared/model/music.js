1a20f448b012d768719620864abe352e47655fc0
leader22 <y.sugiura.0316@gmail.com>
2017-02-22 22:04:01 +0900

    show artist name

--- src/script/shared/model/music.js
+++ src/script/shared/model/music.js
@@ -8,22 +8,28 @@ class MusicModel {
   }
 
   getSearchResult(keyword: string): SearchResultType {
-    const artists = [];
-    const albums = [];
-
     if (keyword.length === 0) {
-      return { artists, albums };
+      return {
+        artists: [],
+        albums: [],
+      };
     }
 
+    const artists = [];
+    const albums = [];
     const reg = new RegExp(keyword, 'i');
     Object.keys(this.json).forEach(artist => {
       reg.test(artist) && artists.push(artist);
       // XXX: flow-disable-line
       Object.keys(this.json[artist]).forEach(album => {
-        reg.test(album) && albums.push(album);
+        reg.test(album) && albums.push(`${artist} - ${album}`);
       });
     });
-    return { artists, albums };
+
+    return {
+      artists: artists.sort(),
+      albums: albums.sort(),
+    };
   }
 }
 
