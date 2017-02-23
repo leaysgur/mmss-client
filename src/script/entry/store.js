c5015528cedae10835e7ac1098dc0d3a80c8f4ad
leader22 <y.sugiura.0316@gmail.com>
2017-02-23 14:07:42 +0900

    rm non-used

--- src/script/entry/store.js
+++ src/script/entry/store.js
@@ -5,21 +5,16 @@ import {
 } from 'mobx';
 
 import SearchObject from './object/search';
-import type {
-  SearchObjectType,
-  SearchResultType,
-} from './object/search';
+
+import type { SearchObjectType, } from './object/search';
 
 
 class EntryStore {
   searchObject: SearchObjectType;
-
   hasLoginError: boolean;
-  searchResults: SearchResultType;
 
   constructor(json: JSON) {
     this.searchObject = new SearchObject(json);
-    this.searchResults = this.searchObject.results;
 
     extendObservable(this, {
       hasLoginError: false,
