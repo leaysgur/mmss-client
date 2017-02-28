65d1669c5d7918a7e04b36a241e87e82544ae763
leader22 <y.sugiura.0316@gmail.com>
2017-02-28 14:59:37 +0900

    styling finder wip

--- src/script/mmss/container/finder.jsx
+++ src/script/mmss/container/finder.jsx
@@ -37,58 +37,62 @@ class Finder extends React.Component {
 
     return (
       <div className="Finder">
-        <div className="Finder_Column">
-          <p>
-            # Artists
-            <button type="button" onClick={onClickSortArtist}>sort</button>
-          </p>
-          <div className="Scroller">
-            <ul className="Scroller_Inner">
-              { artists.map(artist => (
-              <li key={artist.name}>
-                <ArtistItem
-                  item={artist}
-                  onClick={onClickArtist}
-                  onClickPlay={onClickPlayArtist}
-                />
-              </li>
-              )) }
-            </ul>
+        <div className="Finder_Index">
+          <div className="Finder_Column">
+            <p>
+              # Artists
+              <button type="button" onClick={onClickSortArtist}>sort</button>
+            </p>
+            <div className="Scroller">
+              <ul className="Scroller_Inner">
+                { artists.map(artist => (
+                <li key={artist.name}>
+                  <ArtistItem
+                    item={artist}
+                    onClick={onClickArtist}
+                    onClickPlay={onClickPlayArtist}
+                  />
+                </li>
+                )) }
+              </ul>
+            </div>
+          </div>
+
+          <div className="Finder_Column">
+            <p># Albums</p>
+            <div className="Scroller">
+              <ul className="Scroller_Inner">
+                { albums.length === 0 && <li>Artist not selected</li> }
+                { albums.map(album => (
+                <li key={album.name}>
+                  <AlbumItem
+                    item={album}
+                    onClick={onClickAlbum}
+                    onClickPlay={onClickPlayAlbum}
+                  />
+                </li>
+                )) }
+              </ul>
+            </div>
           </div>
         </div>
 
-        <div className="Finder_Column">
-          <p># Albums</p>
-          <div className="Scroller">
-            <ul className="Scroller_Inner">
-              { albums.length === 0 && <li>Artist not selected</li> }
-              { albums.map(album => (
-              <li key={album.name}>
-                <AlbumItem
-                  item={album}
-                  onClick={onClickAlbum}
-                  onClickPlay={onClickPlayAlbum}
-                />
-              </li>
-              )) }
-            </ul>
-          </div>
-        </div>
-
-        <div className="Finder_Column">
-          <p># Songs</p>
-          <div className="Scroller">
-            <ul className="Scroller_Inner">
-              { songs.length === 0 && <li>Album not selected</li> }
-              { songs.map(song => (
-              <li key={song.name}>
-                <SongItem
-                  item={song}
-                  onClickPlay={onClickPlaySong}
-                />
-              </li>
-              )) }
-            </ul>
+        <div className="Finder_Detail">
+          <div className="Finder_Column">
+            <p># Songs</p>
+            <div className="Scroller">
+              <ul className="Scroller_Inner">
+                { songs.length === 0 && <li>Album not selected</li> }
+                { songs.map(song => (
+                <li key={song.name}>
+                  <SongItem
+                    item={song}
+                    onClickPlay={onClickPlaySong}
+                  />
+                </li>
+                )) }
+              </ul>
+            </div>
           </div>
         </div>
       </div>
