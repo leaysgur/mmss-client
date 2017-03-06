8c8a2b758d389bbb48652998f61713d50c75f62b
leader22 <y.sugiura.0316@gmail.com>
2017-03-06 09:21:29 +0900

    use mouseenter to select item

--- src/script/mmss/component/finder/album-item.jsx
+++ src/script/mmss/component/finder/album-item.jsx
@@ -3,32 +3,60 @@ import React from 'react';
 import { observer } from 'mobx-react';
 
 
-const AlbumItem = ({
-  item,
-  isSelected,
-  onClick,
-  onClickPlay,
-}: {
-  item: Album;
-  isSelected: boolean;
-  onClick: (item: Album) => void;
-  onClickPlay: (item: Album) => void;
-}) => (
-  <div className={`AlbumItem ${isSelected ? '-selected' : ''}`} onClick={() => { onClick(item); }}>
-    <a className="AlbumItem_Action" href="#" onClick={(ev) => {
-      ev.preventDefault();
-      ev.stopPropagation();
-      onClickPlay(item);
-    }}>[play]</a>
-    <div className="AlbumItem_Body">
-      <div>
-        {item.name}
+class AlbumItem extends React.Component {
+  el: HTMLDivElement;
+  _handleMouseEnter: () => void;
+  props: {
+    item: Album;
+    isSelected: boolean;
+    onClick: (item: Album) => void;
+    onClickPlay: (item: Album) => void;
+  };
+
+  constructor() {
+    super();
+
+    this._handleMouseEnter = () => {
+      this.props.onClick(this.props.item);
+    };
+  }
+
+  render() {
+    const {
+      item,
+      isSelected,
+      onClickPlay,
+    } = this.props;
+
+    return (
+      <div
+        className={`AlbumItem ${isSelected ? '-selected' : ''}`}
+        ref={ref => { this.el = ref; }}
+      >
+        <a className="AlbumItem_Action" href="#" onClick={(ev) => {
+          ev.preventDefault();
+          ev.stopPropagation();
+          onClickPlay(item);
+        }}>[play]</a>
+        <div className="AlbumItem_Body">
+          <div>
+            {item.name}
+          </div>
+          <div className="AlbumItem_Body_Sub">
+            {item.year || '-'} / {item.songs.length} song(s)
+          </div>
+        </div>
       </div>
-      <div className="AlbumItem_Body_Sub">
-        {item.year || '-'} / {item.songs.length} song(s)
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+
+  componentDidMount() {
+    this.el.addEventListener('mouseenter', this._handleMouseEnter, false);
+  }
+
+  componentWillUnmount() {
+    this.el.removeEventListener('mouseenter', this._handleMouseEnter, false);
+  }
+}
 
 export default observer(AlbumItem);
--- src/script/mmss/component/finder/artist-item.jsx
+++ src/script/mmss/component/finder/artist-item.jsx
@@ -3,32 +3,60 @@ import React from 'react';
 import { observer } from 'mobx-react';
 
 
-const ArtistItem = ({
-  item,
-  isSelected,
-  onClick,
-  onClickPlay,
-}: {
-  item: Artist;
-  isSelected: boolean;
-  onClick: (item: Artist) => void;
-  onClickPlay: (item: Artist) => void;
-}) => (
-  <div className={`ArtistItem ${isSelected ? '-selected' : ''}`} onClick={() => { onClick(item); }}>
-    <a className="ArtistItem_Action" href="#" onClick={(ev) => {
-      ev.preventDefault();
-      ev.stopPropagation();
-      onClickPlay(item);
-    }}>[play]</a>
-    <div className="ArtistItem_Body">
-      <div>
-      {item.name}
+class ArtistItem extends React.Component {
+  el: HTMLDivElement;
+  _handleMouseEnter: () => void;
+  props: {
+    item: Artist;
+    isSelected: boolean;
+    onClick: (item: Artist) => void;
+    onClickPlay: (item: Artist) => void;
+  };
+
+  constructor() {
+    super();
+
+    this._handleMouseEnter = () => {
+      this.props.onClick(this.props.item);
+    };
+  }
+
+  render() {
+    const {
+      item,
+      isSelected,
+      onClickPlay,
+    } = this.props;
+
+    return (
+      <div
+        ref={(ref) => { this.el = ref; }}
+        className={`ArtistItem ${isSelected ? '-selected' : ''}`}
+      >
+        <a className="ArtistItem_Action" href="#" onClick={(ev) => {
+          ev.preventDefault();
+          ev.stopPropagation();
+          onClickPlay(item);
+        }}>[play]</a>
+        <div className="ArtistItem_Body">
+          <div>
+          {item.name}
+          </div>
+          <div className="ArtistItem_Body_Sub">
+            {item.albums.length} album(s)
+          </div>
+        </div>
       </div>
-      <div className="ArtistItem_Body_Sub">
-        {item.albums.length} album(s)
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+
+  componentDidMount() {
+    this.el.addEventListener('mouseenter', this._handleMouseEnter, false);
+  }
+
+  componentWillUnmount() {
+    this.el.removeEventListener('mouseenter', this._handleMouseEnter, false);
+  }
+}
 
 export default observer(ArtistItem);
