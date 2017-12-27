a996c3f153100b50889efc29abd7c6aa3cddb3b3
Yuji Sugiura <y.sugiura.0316@gmail.com>
2017-12-27 11:45:37 +0900

    Split audio

--- src/script/mmss/container/player.jsx
+++ src/script/mmss/container/player.jsx
@@ -2,91 +2,66 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 
 import ProgressBar from '../component/player/progress-bar.jsx';
+import Audio from '../component/player/audio.jsx';
 
-class Player extends React.Component {
-  constructor() {
-    super();
+const Player = ({
+  event,
+  media,
+  playlist,
+  ui,
+}) => {
+  const {
+    onClickPrev, onClickNext,
+    onClickNowPlaying,
+    onMouseEnterPlayer,
+    onMouseLeavePlayer,
+    onEndedMedia,
+  } = event;
+  const { nowPlaying } = playlist;
+  const { currentSrc } = media;
+  const { isMediaLoading, loadProgress } = ui;
+  const isControlable = currentSrc && !isMediaLoading;
 
-    this._handleKeyDown = ev => {
-      if (ev.keyCode !== 32) {
-        return;
-      }
-      if (this.audioEl instanceof HTMLAudioElement === false) {
-        return;
-      }
-      if (this.audioEl.src.length === 0) {
-        return;
-      }
+  return (
+    <div
+      className="Player"
+      onMouseEnter={onMouseEnterPlayer}
+      onMouseLeave={onMouseLeavePlayer}
+    >
+      <div className="Player_ProgressBar">
+        <ProgressBar loadProgress={loadProgress} />
+      </div>
 
-      ev.preventDefault();
-      this.audioEl.paused ? this.audioEl.play() : this.audioEl.pause();
-    };
-  }
+      <div className="Player_Info" onClick={onClickNowPlaying}>
+        {nowPlaying ? `${nowPlaying.artist} - ${nowPlaying.name}` : '-'}
+      </div>
 
-  render() {
-    const {
-      onClickPrev, onClickNext,
-      onClickNowPlaying,
-      onMouseEnterPlayer,
-      onMouseLeavePlayer,
-      onEndedMedia,
-    } = this.props.event;
-    const { nowPlaying } = this.props.playlist;
-    const { currentSrc } = this.props.media;
-    const { isMediaLoading, loadProgress } = this.props.ui;
-    const isControlable = currentSrc && !isMediaLoading;
-
-    return (
-      <div
-        className="Player"
-        onMouseEnter={onMouseEnterPlayer}
-        onMouseLeave={onMouseLeavePlayer}
-      >
-        <div className="Player_ProgressBar">
-          <ProgressBar loadProgress={loadProgress} />
-        </div>
-
-        <div className="Player_Info" onClick={onClickNowPlaying}>
-          {nowPlaying ? `${nowPlaying.artist} - ${nowPlaying.name}` : '-'}
-        </div>
-
-        <div className={`Player_Controls ${isControlable ? '' :  '-disabled'}`}>
-          <a
-            className="Player_Controls_Item"
-            onClick={() => {
-              isControlable && onClickPrev();
-            }}
-          >
-            <img src="/image/i-backward.png" />
-          </a>
-          <a
-            className="Player_Controls_Item"
-            onClick={() => {
-              isControlable && onClickNext();
-            }}
-          >
-            <img src="/image/i-forward.png" />
-          </a>
-          <audio
-            ref={el => this.audioEl = el}
-            className="Player_Controls_Audio"
-            autoPlay
-            controls
+      <div className={`Player_Controls ${isControlable ? '' :  '-disabled'}`}>
+        <a
+          className="Player_Controls_Item"
+          onClick={() => {
+            isControlable && onClickPrev();
+          }}
+        >
+          <img src="/image/i-backward.png" />
+        </a>
+        <a
+          className="Player_Controls_Item"
+          onClick={() => {
+            isControlable && onClickNext();
+          }}
+        >
+          <img src="/image/i-forward.png" />
+        </a>
+        <div className="Player_Controls_Audio">
+          <Audio
             src={currentSrc}
             onEnded={onEndedMedia}
           />
         </div>
       </div>
-    );
-  }
-
-  componentDidMount() {
-    window.addEventListener('keydown', this._handleKeyDown, false);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this._handleKeyDown, false);
-  }
-}
+    </div>
+  );
+};
 
 export default inject('event')(observer(Player));
